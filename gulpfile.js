eeb844b9aad0aa1805a33ca006efe0db30970435
dalabarge <daniel@bexarcreative.com>
2015-02-16 18:23:24 -0600

    Cleaned up files to be brought more inline with esensi/esensi.

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+/* jshint node: true */
+
+/**
+ * Gulp Tasks
+ *
+ * @package Esensi\Build
+ * @author daniel <dalabarge@emersonmedia.com>
+ * @author matt <matt@emersonmedia.com>
+ * @copyright 2014 Emerson Media LP
+ * @license https://github.com/esensi/build/blob/master/LICENSE.txt MIT License
+ * @link http://www.emersonmedia.com
+ */
+
+"use strict";
+
+global.configOpts = require('./build.json');
+require('esensi-build');
