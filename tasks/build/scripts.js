2cf0ec4e9839b2cd77cc76f5107f726caf86d33a
Daniel LaBarge <daniel@bexarcreative.com>
2015-04-25 02:29:02 -0500

    It's ugly but it works. Will clean up later.

--- tasks/build/scripts.js
+++ tasks/build/scripts.js
@@ -11,7 +11,7 @@
  *
  * Note that the default behavior for build:scripts is to run clean:scripts
  * first. Later on, if you start to use watchify, this may be undesirable.
- * 
+ *
  * Among other things, this task was based on:
  * https://medium.com/@sogko/gulp-browserify-the-gulp-y-way-bb359b3f9623
  *
@@ -26,8 +26,10 @@ var browserify = require('browserify');
 var gulpif     = require('gulp-if');
 var rev        = require('gulp-rev');
 var sourcemaps = require('gulp-sourcemaps');
-var transform  = require('vinyl-transform');
+var stream     = require('vinyl-source-stream');
 var uglify     = require('gulp-uglify');
+var buffer     = require('vinyl-buffer');
+var glob       = require('glob');
 var config     = global.configOpts;
 
 gulp.task('build:scripts', ['clean:scripts'], function() {
@@ -35,35 +37,38 @@ gulp.task('build:scripts', ['clean:scripts'], function() {
     var source = config.scripts.source;
     var dest   = config.scripts.dest;
 
-    var browserify_options = {
-        debug: !global.is_production,
-        insertGlobals: true,
-        detectGlobals: true,
-        noParse: false,
-    };
+    var pipeline;
+    glob(source, {}, function(err, files)
+    {
+        files.forEach(function(filename)
+        {
+            var name = filename.split('/');
+            name = name[name.length - 1];
+            pipeline = browserify(filename, {
+                debug: ! global.is_production,
+                insertGlobals: true,
+                detectGlobals: true,
+                noParse: false
+            }).bundle()
+            .pipe(stream(name))
+            .pipe(buffer())
+            .pipe(gulpif(!global.is_production, sourcemaps.init({loadMaps: true}))) // load map from browserify file
+            .pipe(gulpif(global.is_production, uglify())) // Uglify if production
+            .pipe(gulpif(!global.is_production, sourcemaps.write('.'))) // Write maps externally to same directory
 
-    // Use transform to convert the regular text Stream that browserify emits
-    // into a vinyl file object that we can push into a gulp pipe later
-    var browserified = transform(function(filename) {
-        var b = browserify(filename, browserify_options);
-        return b.bundle();
+            // Save original
+            .pipe(gulpif(!config.revisions, gulp.dest(dest)))
+
+            // Build revisions
+            .pipe(gulpif(config.revisions, rev()))
+            .pipe(gulpif(config.revisions, gulp.dest(dest))) // write rev'd assets to build dir
+
+            // Build revisions manifest
+            .pipe(gulpif(config.revisions, rev.manifest()))
+            .pipe(gulpif(config.revisions, gulp.dest(dest))); // write manifest to build dir
+        });
     });
 
-    return gulp.src(source)
-        .pipe(browserified)
-        .pipe(gulpif(!global.is_production, sourcemaps.init({loadMaps: true}))) // load map from browserify file
-        .pipe(gulpif(global.is_production, uglify())) // Uglify if production
-        .pipe(gulpif(!global.is_production, sourcemaps.write('.'))) // Write maps externally to same directory
-
-        // Save original
-        .pipe(gulpif(!config.revisions, gulp.dest(dest)))
-
-        // Build revisions
-        .pipe(gulpif(config.revisions, rev()))
-        .pipe(gulpif(config.revisions, gulp.dest(dest))) // write rev'd assets to build dir
-
-        // Build revisions manifest
-        .pipe(gulpif(config.revisions, rev.manifest()))
-        .pipe(gulpif(config.revisions, gulp.dest(dest))); // write manifest to build dir
+    return pipeline;
 
 });
